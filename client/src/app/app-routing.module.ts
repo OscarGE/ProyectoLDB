f956ff8aaee47632433942da676cb5840db3529f
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2021-12-06 17:43:46 -0600

    adding routes
    -Add Routes to nav

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './components/Tabs/products/products.component';
+import { SalesComponent } from './components/Tabs/sales/sales.component';
+import { SummaryComponent } from './components/Tabs/summary/summary.component';
+import { SuppliersComponent } from './components/Tabs/suppliers/suppliers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'suppliers', component: SuppliersComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'sales', component: SalesComponent},
+  {path: 'summary', component: SummaryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
+import { SuppliersComponent } from './components/Tabs/suppliers/suppliers.component';
+import { ProductsComponent } from './components/Tabs/products/products.component';
+import { SalesComponent } from './components/Tabs/sales/sales.component';
+import { SummaryComponent } from './components/Tabs/summary/summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    UserFormComponent
+    UserFormComponent,
+    SuppliersComponent,
+    ProductsComponent,
+    SalesComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
