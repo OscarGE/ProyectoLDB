b0a91c964777de606819b55ca5efcd0e64890613
OscarGE <oscarge199902@gmail.com>
2021-12-06 16:12:38 -0600

    Primer commit

---
+++ server/build/keys.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// interfaz para alamcenar la info que perimite la conexión con la base de datos
+exports.default = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'crm_abarrotes', //Nombre de la base de datos
+    }
+};
---
+++ server/src/keys.ts
@@ -0,0 +1,9 @@
+// interfaz para alamcenar la info que perimite la conexión con la base de datos
+export default{
+  database: {
+    host: 'localhost', //Dirección ip de alojamiento 
+    user: 'root',
+    password: '',
+    database: 'crm_abarrotes', //Nombre de la base de datos
+  }
+}
